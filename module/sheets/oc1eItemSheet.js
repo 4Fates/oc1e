615549c7e038ed4de74ec64d696a51ecf66a44b8
4Fates <shadowzack654@gmail.com>
2022-03-17 16:32:23 -0400

    commit 2

--- module/sheets/oc1eItemSheet.js
+++ module/sheets/oc1eItemSheet.js
@@ -1,5 +1,5 @@
 export default class oc1eItemSheet extends Itemsheet {
     get template() {
-        return `systems/oc1e/templates/sheets/${this.item.data.type} -sheet.html`;
+        return `systems/oc1e/templates/sheets/${this.item.data.type}-sheet.html`;
     }
 }
--- templates/sheets/oc1e.js
+++ templates/sheets/oc1e.js
@@ -1,8 +1,8 @@
-import oc1eItemSheet from "../../module/sheets/oc1eItemSheet";
+import oc1eItemSheet from "./module/sheets/oc1eItemSheet.js";
 
 Hooks.once("init", function() {
     console.log("oc1e | Initalising Occovar System");
 
-    oc1eItemSheet.unregisterSheet("core", ItemSheet)
-    oc1eItemSheet.registerSheet("oc1e", oc1eItemSheet, {makeDefault:});
+    oc1eItemSheet.unregisterSheet("core", ItemSheet) ;
+    oc1eItemSheet.registerSheet("oc1e", oc1eItemSheet, {makeDefault: true});
 })
